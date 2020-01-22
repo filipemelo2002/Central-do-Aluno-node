bb0ff2942d6c3ccb89b52ab8c54c3d39c8e19654
mrnobody2002 <filipemelo032@gmail.com>
2020-01-21 21:44:33 -0300

    Handle CORS

--- index.js
+++ index.js
@@ -4,12 +4,14 @@
 const express = require('express')
 const routes = require('./src/routes')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const app = express()
 
 mongoose.connect('mongodb+srv://centraldoaluno:centraldoaluno@cluster0-6nhph.mongodb.net/CentraldoAluno?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 const port = process.env.PORT || 3000
