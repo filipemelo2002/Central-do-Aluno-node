673706694f3a1d2aa9730cf5f077dccc7b997888
mrnobody2002 <filipemelo032@gmail.com>
2020-02-21 10:16:20 -0300

    Horarios ednpoint returning properly

--- src/controllers/HorariosController.js
+++ src/controllers/HorariosController.js
@@ -22,6 +22,46 @@ function getStringBetween(str, prefix, suffix) {
 	}
 	return s;
 };
+var getFromBetween = {
+    results:[],
+    string:"",
+    getFromBetween:function (sub1,sub2) {
+        if(this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return false;
+        var SP = this.string.indexOf(sub1)+sub1.length;
+        var string1 = this.string.substr(0,SP);
+        var string2 = this.string.substr(SP);
+        var TP = string1.length + string2.indexOf(sub2);
+        return this.string.substring(SP,TP);
+    },
+    removeFromBetween:function (sub1,sub2) {
+        if(this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return false;
+        var removal = sub1+this.getFromBetween(sub1,sub2)+sub2;
+        this.string = this.string.replace(removal,"");
+    },
+    getAllResults:function (sub1,sub2) {
+        // first check to see if we do have both substrings
+        if(this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return;
+
+        // find one result
+        var result = this.getFromBetween(sub1,sub2);
+        // push it to the results array
+        this.results.push(result);
+        // remove the most recently found one from the string
+        this.removeFromBetween(sub1,sub2);
+
+        // if there's more substrings
+        if(this.string.indexOf(sub1) > -1 && this.string.indexOf(sub2) > -1) {
+            this.getAllResults(sub1,sub2);
+        }
+        else return;
+    },
+    get:function (string,sub1,sub2) {
+        this.results = [];
+        this.string = string;
+        this.getAllResults(sub1,sub2);
+        return this.results;
+    }
+};
 async function getHorariosTable(cookies){
     let response = await api.get(
         `/quadrodehorarios/DetalharQuadroHorarioPortal.do`,{
@@ -48,8 +88,46 @@ async function getHorariosTable(cookies){
     if(!response){
         return false
     }
-
+    
     return response.data
+}
+
+function getHorarioAsJson(htmlTable){
+    const table =  getStringBetween(htmlTable, '<div class="TabelaHorarios">', '</div>')
+    const tableBody = getStringBetween(table, '<tbody>', '</tbody>')
+    const tableColumns = getFromBetween.get(tableBody, '<tr class', '</tr>')
+
+    const tableRows  = []
+
+    tableColumns.forEach(element => {
+        tableRows.push(
+            getFromBetween.get(element,  '<td>', '</td>')
+        )
+    });
+    if(tableRows[0]){
+        const week = ['seg','ter','quar','quin', 'sext', 'sab', 'domi']
+        const response = []
+        
+        response.push({
+            seg: "Seg",
+            ter: "Terç",
+            quar: "Qua",
+            quin: "Qui",
+            sext: "Sex",
+            sab: "Sáb",
+            domi: "Dom",
+        })
+        
+        for(let i=0; i< tableRows.length; i++){
+            const row = {}
+            for(let j=0; j < tableRows[i].length; j++){
+                row[week[j]] =  tableRows[i][j]
+            }
+            response.push(row)
+        }
+        return response
+    }
+    return false
 }
 module.exports = {
     async index(request, response){
@@ -67,8 +145,8 @@ module.exports = {
             })
         }
 
-        //const horarioHtmlTable = await getHorariosTable(session)
-
-        return response.send({message:"Serviço indisponível no momento"})
+        const horarioHtmlTable = await getHorariosTable(session)
+        const horario = getHorarioAsJson(horarioHtmlTable)
+        return response.json(horario)
     }
 }
