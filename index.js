e8d85ecc779c85691d1b03c32557eba693187ca3
mrnobody2002 <filipemelo032@gmail.com>
2020-04-03 18:04:30 -0300

    index file changed

--- index.js
+++ index.js
@@ -1,21 +1,6 @@
-'use strict'
+const app = require('./src/app')
 
-
-const express = require('express')
-const routes = require('./src/routes')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const app = express()
-
-mongoose.connect('mongodb+srv://centraldoaluno:centraldoaluno@cluster0-6nhph.mongodb.net/CentraldoAluno?retryWrites=true&w=majority',{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use(routes)
 const port = process.env.PORT || 3000
-app.listen(port, function(){
-    console.log('Your node js server is running at '+port);
+app.listen(port, function () {
+  console.log('Your node js server is running at ' + port)
 })
