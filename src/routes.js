34626a9d657c2dfc083ce9dbc813df878db1f03a
mrnobody2002 <filipemelo032@gmail.com>
2020-04-03 18:05:51 -0300

    Auth middlerwares added to routes

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,23 @@
 'use strict'
 
-const {Router} = require('express')
+const { Router } = require('express')
 
 const routes = Router()
-
+const AuthUser = require('./middlewares/auth')
+const AuxFunctions = require('./jobs/AuxFunctions')
 const SessionsController = require('./controllers/SessionsController')
 const BoletinController = require('./controllers/BoletinController')
 const FaltasController = require('./controllers/FaltasController')
 const HorariosController = require('./controllers/HorariosController')
-routes.get('/',(req, res)=>{
-    return res.json({
-        message:"Opa, e aew?"
-    })
+routes.get('/', (req, res) => {
+  return res.json({
+    message: 'Opa, e aew?'
+  })
 })
 
-
 routes.post('/sessions', SessionsController.index)
-routes.get('/boletins', BoletinController.index)
-routes.get('/boletins/view', BoletinController.show)
-routes.get('/faltas',FaltasController.index)
-routes.get('/horarios',HorariosController.index)
+routes.get('/boletins', AuthUser.authUser, BoletinController.index)
+routes.get('/boletins/view', AuthUser.authUser, AuxFunctions.handleParameters, BoletinController.show)
+routes.get('/faltas', AuthUser.authUser, AuxFunctions.handleParameters, FaltasController.index)
+routes.get('/horarios', AuthUser.authUser, HorariosController.index)
 module.exports = routes
