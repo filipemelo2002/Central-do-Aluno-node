06642f1861be087132f282ec3fc8ad5536a2087d
mrnobody2002 <filipemelo032@gmail.com>
2020-04-03 18:08:35 -0300

    New Enviroment Pattern (indentation)

--- src/utils/SessionsHandler.js
+++ src/utils/SessionsHandler.js
@@ -4,80 +4,79 @@ const qs = require('querystring')
 
 const User = require('../models/UserModel')
 
-async function handleLogin(login, senha){
-    const formBody = {
-        login,
-        senha
-    }
-    const response = await api.post(
-        '/GerenciadorAcessoWeb/segurancaAction.do?actionType=ajaxLogin&dummy=',
-        qs.stringify(formBody))
+async function handleLogin (login, senha) {
+  const formBody = {
+    login,
+    senha
+  }
+  const response = await api.post(
+    '/GerenciadorAcessoWeb/segurancaAction.do?actionType=ajaxLogin&dummy=',
+    qs.stringify(formBody))
 
-    return response
+  return response
 }
 
-async function getUserCredentials(userToken){
-    const user = await User.findOne({userToken})
-    if(user){
-        return user
-    }
-    return false
+async function getUserCredentials (userToken) {
+  const user = await User.findOne({ userToken })
+  if (user) {
+    return user
+  }
+  return false
 }
-function getHeadersToReturnJSession(loginSubscription){
-    const {headers} = loginSubscription
-    return headers['set-cookie']
+function getHeadersToReturnJSession (loginSubscription) {
+  const { headers } = loginSubscription
+  return headers['set-cookie']
 }
 module.exports = {
 
-    async handleLogin(login, senha){
-        const formBody = {
-            login,
-            senha
-        }
-        const response = await api.post(
-            '/GerenciadorAcessoWeb/segurancaAction.do?actionType=ajaxLogin&dummy=',
-            qs.stringify(formBody))
-    
-        return response
-    },
-    async userTokenHandler(userToken){
-
-        const userAuthorized = await getUserCredentials(userToken)
-        if(!userAuthorized){
-            return false
-        }
-        const {email, senha} = userAuthorized
-
-        const loginSubscription = await handleLogin(email, senha)
-        if(!loginSubscription){
-            return false
-        }
-        const session = getHeadersToReturnJSession(loginSubscription)
-        return session
-    },
-    async  getTurmaId(cookies, boletimId){
-        const response = await api.get(
-            `/ws/eol/aluno/documentos/BoletimEscolar/alunoMatricula?idAlunoMatricula=${boletimId}&isInterface=true&request.preventCache=`,{
-            headers:{
-                Cookie: `${cookies[0]};${cookies[1]}`
-            }
-        })
-    
-        if(!response){
-            return false
-        }
-        return response.data[0]
-    },
-    async getBoletinData(cookies, boletimId, id_turma, ano){
-        const response = await api.get(
-            `/ws/eol/aluno/documentos/BoletimEscolar/componenteCurricular?idAlunoMatricula=${boletimId}&idTurma=${id_turma}&ano=${ano}&isInterface=true&request.preventCache=`,{
-            headers:{
-                Cookie: `${cookies[0]};${cookies[1]}`
-            }
-        })
-        if(!response){
-            return false
-        }
-        return response.data
+  async handleLogin (login, senha) {
+    const formBody = {
+      login,
+      senha
     }
+    const response = await api.post(
+      '/GerenciadorAcessoWeb/segurancaAction.do?actionType=ajaxLogin&dummy=',
+      qs.stringify(formBody))
+
+    return response
+  },
+  async userTokenHandler (userToken) {
+    const userAuthorized = await getUserCredentials(userToken)
+    if (!userAuthorized) {
+      return false
+    }
+    const { email, senha } = userAuthorized
+
+    const loginSubscription = await handleLogin(email, senha)
+    if (!loginSubscription) {
+      return false
+    }
+    const session = getHeadersToReturnJSession(loginSubscription)
+    return session
+  },
+  async  getTurmaId (cookies, boletimId) {
+    const response = await api.get(
+            `/ws/eol/aluno/documentos/BoletimEscolar/alunoMatricula?idAlunoMatricula=${boletimId}&isInterface=true&request.preventCache=`, {
+              headers: {
+                Cookie: `${cookies[0]};${cookies[1]}`
+              }
+            })
+
+    if (!response) {
+      return false
+    }
+    return response.data[0]
+  },
+  async getBoletinData (cookies, boletimId, id_turma, ano) {
+    const response = await api.get(
+            `/ws/eol/aluno/documentos/BoletimEscolar/componenteCurricular?idAlunoMatricula=${boletimId}&idTurma=${id_turma}&ano=${ano}&isInterface=true&request.preventCache=`, {
+              headers: {
+                Cookie: `${cookies[0]};${cookies[1]}`
+              }
+            })
+    if (!response) {
+      return false
+    }
+    return response.data
+  }
 }
