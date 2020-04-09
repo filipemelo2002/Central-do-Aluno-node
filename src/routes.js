47b8e4f8b5167824b0fba8180a9ca5f31bb5e89f
mrnobody2002 <filipemelo032@gmail.com>
2020-04-08 21:15:34 -0300

    privacy policy

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const { Router } = require('express')
-
+const path = require('path')
 const routes = Router()
 const AuthUser = require('./middlewares/auth')
 const AuxFunctions = require('./jobs/AuxFunctions')
@@ -17,7 +17,20 @@ routes.get('/', (req, res) => {
 
 routes.post('/sessions', SessionsController.index)
 routes.get('/boletins', AuthUser.authUser, BoletinController.index)
-routes.get('/boletins/view', AuthUser.authUser, AuxFunctions.handleParameters, BoletinController.show)
-routes.get('/faltas', AuthUser.authUser, AuxFunctions.handleParameters, FaltasController.index)
+routes.get(
+  '/boletins/view',
+  AuthUser.authUser,
+  AuxFunctions.handleParameters,
+  BoletinController.show
+)
+routes.get(
+  '/faltas',
+  AuthUser.authUser,
+  AuxFunctions.handleParameters,
+  FaltasController.index
+)
 routes.get('/horarios', AuthUser.authUser, HorariosController.index)
+routes.get('/policy', (req, res) => {
+  return res.sendFile(path.resolve(__dirname, 'views', 'policy.html'))
+})
 module.exports = routes
